94256b5e548594a364dd8569876221cafc81a118
Matt <mbeimcik@gmail.com>
2018-05-31 09:35:01 +0200

    step 5 paid flavor complete

--- andjoker/src/main/java/com/leadinsource/andjoker/JokeDisplay.java
+++ andjoker/src/main/java/com/leadinsource/andjoker/JokeDisplay.java
@@ -1,11 +1,9 @@
 package com.leadinsource.andjoker;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class JokeDisplay extends AppCompatActivity {
 
