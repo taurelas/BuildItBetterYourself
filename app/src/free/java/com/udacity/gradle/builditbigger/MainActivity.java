5a9b2d076f1e41de4ebd4099d02dfa89024726eb
Matt <mbeimcik@gmail.com>
2018-05-31 09:56:48 +0200

    step 6 - interstitial ad ok

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -6,17 +6,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
 import com.leadinsource.andjoker.JokeDisplay;
 import com.leadinsource.joker.Joker;
 
 
 public class MainActivity extends AppCompatActivity implements ActivityCallback {
 
+    private InterstitialAd interstitialAd;
+    private boolean useApi = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MobileAds.initialize(this, "ca-app-pub-3940256099942544/1033173712");
+        interstitialAd = new InterstitialAd(this);
+        interstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        interstitialAd.loadAd(new AdRequest.Builder().build());
+        interstitialAd.setAdListener(new AdListener() {
+
+            @Override
+            public void onAdClosed() {
+                interstitialAd.loadAd(new AdRequest.Builder().build());
+                if(useApi) {
+                    new EndpointsAsyncTask().execute(MainActivity.this);
+                } else {
+                    Intent intent = new Intent(MainActivity.this, JokeDisplay.class);
+                    intent.putExtra(JokeDisplay.JOKE_KEY, Joker.getJoke());
+                    startActivity(intent);
+                }
+            }
+        });
+
     }
 
 
@@ -43,15 +71,25 @@ public class MainActivity extends AppCompatActivity implements ActivityCallback 
     }
 
     public void tellJoke(View view) {
-        Intent intent = new Intent(this, JokeDisplay.class);
-        intent.putExtra(JokeDisplay.JOKE_KEY, Joker.getJoke());
-        startActivity(intent);
+        useApi = false;
+        if(interstitialAd.isLoaded()) {
+            interstitialAd.show();
+        } else {
+            Toast.makeText(this, "Try again later or use paid version :) Only £199", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 
     public void tellJokeViaApi(View view) {
+        useApi = true;
+        if(interstitialAd.isLoaded()) {
+            interstitialAd.show();
+        }else {
+            Toast.makeText(this, "Try again later or use paid version :) Only £199", Toast.LENGTH_SHORT).show();
+        }
 
-        new EndpointsAsyncTask().execute(this);
+
     }
 
     public void onResult(String joke) {
